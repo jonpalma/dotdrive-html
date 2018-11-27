5149cdee1643ab2014bbcbd6386f07abdc2a6ff5
Jose Angel Lujan <angel.lujan@orderos.mx>
2018-11-27 15:00:17 -0700

    Added more parts of the accident form

--- accident-add-2.php
+++ accident-add-2.php
@@ -380,6 +380,463 @@
 
     </div>
 
+    <hr class="mb-4">
+
+    <h4 class="page-title">Preliminary Report of Accident or Loss</h4>
+
+    <div class="row">
+
+        <div class="form-group col-md-4">
+            <label>Date of Accident</label>
+            <input type="text" class="form-control date" id="dateofaccident" data-toggle="date-picker"
+                   data-single-date-picker="true" required>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>Time</label>
+            <div class="input-group">
+                <input type="text" class="form-control" data-toggle="timepicker" id="time"
+                       value="10:00:00">
+                <div class="input-group-append">
+                    <span class="input-group-text"><i class="dripicons-clock"></i></span>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>Day of Week</label>
+            <input type="text" id="dayofweek" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-4">
+            <label>Total Number of Vehicles Involved in Accident?</label>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>Company</label>
+            <input type="text" id="vehiclescompany" class="form-control">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>Others</label>
+            <input type="text" id="vehiclesothers" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Company Driver</label>
+            <input type="text" id="companydriver" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Truck or Tractor No.</label>
+            <input type="text" id="truckortractornumber" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Trailer No.</label>
+            <input type="text" id="trailernumber" class="form-control">
+        </div>
+
+        <div class="form-group mt-3 col-md-6">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="citedforviolation">
+                <label class="custom-control-label" for="citedforviolation">Cited for Moving Violation?</label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Phone No.</label>
+            <input type="text" id="phonenumber" class="form-control"
+                   data-toggle="input-mask" data-mask-format="(000) 000-0000"
+                   maxlength="14">
+            <span class="font-13 text-muted">e.g "(xxx) xxx-xxxx"</span>
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>City</label>
+            <input type="text" id="city" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>State</label>
+            <select class="form-control" id="state">
+                <option selected disabled></option>
+                <option>Alabama</option>
+                <option>Alaska</option>
+                <option>Arizona</option>
+                <option>Arkansas</option>
+                <option>California</option>
+                <option>Colorado</option>
+                <option>Connecticut</option>
+                <option>Delaware</option>
+                <option>District of Columbia</option>
+                <option>Florida</option>
+                <option>Georgia</option>
+                <option>Hawaii</option>
+                <option>Idaho</option>
+                <option>Illinois</option>
+                <option>Indiana</option>
+                <option>Iowa</option>
+                <option>Kansas</option>
+                <option>Kentucky</option>
+                <option>Louisiana</option>
+                <option>Maine</option>
+                <option>Maryland</option>
+                <option>Massachusetts</option>
+                <option>Michigan</option>
+                <option>Minnesota</option>
+                <option>Mississippi</option>
+                <option>Missouri</option>
+                <option>Montana</option>
+                <option>Nebraska</option>
+                <option>Nevada</option>
+                <option>New Hampshire</option>
+                <option>New Jersey</option>
+                <option>New Mexico</option>
+                <option>New York</option>
+                <option>North Carolina</option>
+                <option>North Dakota</option>
+                <option>Ohio</option>
+                <option>Oklahoma</option>
+                <option>Oregon</option>
+                <option>Pennsylvania</option>
+                <option>Rhode Island</option>
+                <option>South Carolina</option>
+                <option>South Dakota</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Utah</option>
+                <option>Vermont</option>
+                <option>Virginia</option>
+                <option>Washington</option>
+                <option>West Virginia</option>
+                <option>Wisconsin</option>
+                <option>Wyoming</option>
+            </select>
+        </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <label>Accident Withing City, Town or Village Limits</label>
+
+    <div class="row">
+
+        <div class="form-group col-md-4">
+            <label>City/Town/Village</label>
+            <input type="text" id="withinlimitscity" class="form-control">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>County</label>
+            <input type="text" id="withinlimitscounty" class="form-control">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>State</label>
+            <select class="form-control" id="withinlimitsstate">
+                <option selected disabled></option>
+                <option>Alabama</option>
+                <option>Alaska</option>
+                <option>Arizona</option>
+                <option>Arkansas</option>
+                <option>California</option>
+                <option>Colorado</option>
+                <option>Connecticut</option>
+                <option>Delaware</option>
+                <option>District of Columbia</option>
+                <option>Florida</option>
+                <option>Georgia</option>
+                <option>Hawaii</option>
+                <option>Idaho</option>
+                <option>Illinois</option>
+                <option>Indiana</option>
+                <option>Iowa</option>
+                <option>Kansas</option>
+                <option>Kentucky</option>
+                <option>Louisiana</option>
+                <option>Maine</option>
+                <option>Maryland</option>
+                <option>Massachusetts</option>
+                <option>Michigan</option>
+                <option>Minnesota</option>
+                <option>Mississippi</option>
+                <option>Missouri</option>
+                <option>Montana</option>
+                <option>Nebraska</option>
+                <option>Nevada</option>
+                <option>New Hampshire</option>
+                <option>New Jersey</option>
+                <option>New Mexico</option>
+                <option>New York</option>
+                <option>North Carolina</option>
+                <option>North Dakota</option>
+                <option>Ohio</option>
+                <option>Oklahoma</option>
+                <option>Oregon</option>
+                <option>Pennsylvania</option>
+                <option>Rhode Island</option>
+                <option>South Carolina</option>
+                <option>South Dakota</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Utah</option>
+                <option>Vermont</option>
+                <option>Virginia</option>
+                <option>Washington</option>
+                <option>West Virginia</option>
+                <option>Wisconsin</option>
+                <option>Wyoming</option>
+            </select>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Street</label>
+            <input type="text" id="withinlimitsstreet" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Cross Street</label>
+            <input type="text" id="withinlimitscrosstreet" class="form-control">
+        </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <label>Accident Outside City, Town or Village Limits</label>
+
+    <div class="row">
+
+        <div class="form-group col-md-4">
+            <label>Road Number and Road Name</label>
+            <input type="text" id="outsidelimitsroad" class="form-control">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>Town</label>
+            <input type="text" id="outsidelimitstown" class="form-control">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label>County</label>
+            <input type="text" id="outsidelimitscounty" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Name Intersection or State Distance and Direction from Nearest Community, Highway Junction,
+                Crossroads, Milepost</label>
+            <input type="text" id="outsidelimitintersection" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Total Number of Lanes (Both Directions)</label>
+            <input type="text" id="outsidelimitstotalnumberoflanes" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group mt-3 col-md-6">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="outsidelimitsmarkedlanes">
+                <label class="custom-control-label" for="outsidelimitsmarkedlanes">Were Lanes Marked?</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-6">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="outsidelimitslanesseparated">
+                <label class="custom-control-label" for="outsidelimitslanesseparated">Were Opposing Lanes Separated by
+                    Curb or Median?</label>
+            </div>
+        </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <label>Traffic Control at Scene</label>
+
+    <div class="row">
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="signallight">
+                <label class="custom-control-label" for="signallight">Signal Light</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="stopsign">
+                <label class="custom-control-label" for="stopsign">Stop Sign</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="policeofficer">
+                <label class="custom-control-label" for="policeofficer">Police Officer</label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="warningsign">
+                <label class="custom-control-label" for="warningsign">Warning Sign</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="rrlights">
+                <label class="custom-control-label" for="rrlights">RR Lights/Gates</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="rrcrossbuck">
+                <label class="custom-control-label" for="rrcrossbuck">RR Crossbuck</label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="workzone">
+                <label class="custom-control-label" for="workzone">Work Zone</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="ohtercontrol">
+                <label class="custom-control-label" for="ohtercontrol">Other Control</label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3 col-md-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="nocontrol">
+                <label class="custom-control-label" for="nocontrol">No Control</label>
+            </div>
+        </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <label>Causes of Accident or Loss</label>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Weather Condition</label>
+            <input type="text" id="weathercondition" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Condition of Road</label>
+            <input type="text" id="conditionofroad" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-12">
+            <label for="descriptionofaccident">Description of Accident</label>
+            <textarea class="form-control" id="descriptionofaccident" rows="3"></textarea>
+        </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <label>Driver of Other Vehicle</label>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Name</label>
+            <input type="text" id="otherdrivername" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Make of Vehicle</label>
+            <input type="text" id="otherdrivermakeofvehicle" class="form-control">
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Address</label>
+            <input type="text" id="otherdriveraddress" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Model</label>
+            <input type="text" id="otherdrivermodel" class="form-control">
+        </div>
+
+    </div>
+
+
+    <div class="row">
+
+        <div class="form-group col-md-6">
+            <label>Operator's License No.</label>
+            <input type="text" id="otherdriverlicense" class="form-control">
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Registration</label>
+            <input type="text" id="otherdriverregustration" class="form-control">
+        </div>
+
+    </div>
+
     <div class="row">
 
         <div class="form-group col-12">
