73f924404b8d1f8882958eafc795c738ffddf5b4
Jose Angel Lujan <angel.lujan@orderos.mx>
2018-12-03 12:11:41 -0700

    Accident list tables

--- accident-add-3.php
+++ accident-add-3.php
@@ -6,42 +6,21 @@
         <thead class="thead-dark">
         <tr>
             <th>Id</th>
-            <th>Date</th>
-            <th>Case Name</th>
-            <th>Claim No.</th>
-            <th>Advance</th>
-            <th>Status</th>
-            <th>Job title</th>
-            <th></th>
+            <th>Injury/Illness Date</th>
+            <th>Injury/Illness Name</th>
+            <th>Injury/Illness ID</th>
+            <th>What Employee Was Doing</th>
+            <th>What Happened</th>
         </tr>
         </thead>
         <tbody>
         <tr>
             <td><a href="#">#10</a></td>
             <td>01/11/2018</td>
-            <td>Accident 1</td>
-            <td>800HOO332</td>
-            <td>
-                <div class="progress progress-sm">
-                    <div class="progress-bar progress-lg bg-success" role="progressbar" style="width: 100%"
-                         aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </td>
-            <td><i class="mdi mdi-circle text-success"></i> Complete</td>
-            <td>LLD</td>
-            <td>
-                <div class="dropdown">
-                    <a href="#" class="" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true"
-                       aria-expanded="false">
-                        <i class="mdi mdi-dots-vertical-circle"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <button class="dropdown-item" type="button"><i class="mdi mdi-square-edit-outline"></i> Edit
-                        </button>
-                        <button class="dropdown-item" type="button"><i class="mdi mdi-delete"></i> Delete</button>
-                    </div>
-                </div>
-            </td>
+            <td>Illness</td>
+            <td>Illness ID 1</td>
+            <td>Driving</td>
+            <td>Crash</td>
         </tr>
         </tbody>
     </table>
